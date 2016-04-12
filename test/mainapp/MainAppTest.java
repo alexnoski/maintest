1970659c2276c1635c3f4e16a8c37765cfce8aa0
rich <alexnoski@yahoo.com>
2016-04-12 10:48:24 -0700

    first commit

---
+++ test/mainapp/MainAppTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainapp;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author rich
+ */
+public class MainAppTest {
+    
+    
+    @Test
+    public void testMain() {
+        	assertTrue(1 == 1);
+    }
+    
+}
